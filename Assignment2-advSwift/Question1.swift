6825699ace5228adcca84176bcfe9ecd7865773f
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-06-14 17:47:19 -0400

    Created Function  to Calculate Area of Square

--- Assignment2-advSwift/Question1.swift
+++ Assignment2-advSwift/Question1.swift
@@ -6,6 +6,16 @@
 //
 
 import Foundation
+
+// Answer 1
+// Created a Funcition to Calculate AREA of square
+// Although Square has both side same height = width the question describe parameter so its this way
+func calculateArea(height: Int, width: Int) -> Int {
+    let area = height * width
+    return area
+}
+
+
 /**
  Question 1:
  for this questions you need to create 3 functions
