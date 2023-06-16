5da094a847587d8e1592705ac42f58447dc381c7
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-06-16 15:42:18 -0400

    Added Child class variable

--- Assignment2-advSwift/Question3.swift
+++ Assignment2-advSwift/Question3.swift
@@ -6,38 +6,7 @@
 //
 
 import Foundation
-/**
- create a parent class and a subclass
- 
- PARENT CLASS:
- class name:
- - Computer
- attributes:
- - cpu: String
- - gpu: String
- - ram: Int
- - state: String = "Off"
- - temp: Double
- methods:
- - init(cpu: String, gpu: String, ram: Int, temp: Double)
- - upgradeRam(newAmount: Int)
- - turnOn() => will set the state to 'On'
- - turnOff() => will set the state to 'Off'
- - getTemp() -> Double
- 
- 
- CHILD CLASS:
- class name:
- - Laptop
- attributes:
- - isRamUpgradable: Bool
- - screenSize: Double
- - isTouchScreen: Bool
- methods:
- - init(cpu: String, gpu: String, ram: Int, state: String, temp: Double, isRamUpgradable: Bool, screenSize: Double, isTouchScreen: Bool)
- - override upgradeRam() => if the ram is not upgradable do nothing
- - closeScreen() => this will set the laptop state to 'Sleep'
- */
+// computer describition
 
 class Computer {
     var cpu: String
@@ -50,7 +19,7 @@ class Computer {
         self.cpu = cpu
         self.gpu = gpu
         self.ram = ram
-        self.temp = temp
+        self.temp = temp //temp can be decible as well double
     }
     
     func upgradeRam(newAmount: Int) {
@@ -68,4 +37,12 @@ class Computer {
     func getTemp() -> Double {
         return temp
     }
+}
+
+
+// child class
+class Laptop: Computer {
+    var isRamUpgradable: Bool
+    var screenSize: Double
+    var isTouchScreen: Bool
 }
