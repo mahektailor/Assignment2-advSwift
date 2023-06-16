a3c2aab3ade1dac8078cc2750b09b78314b4cd76
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-06-16 02:28:42 -0400

    Added Variable to get user Input from user to call function allCap

--- Assignment2-advSwift/main.swift
+++ Assignment2-advSwift/main.swift
@@ -16,6 +16,18 @@ let area = calculateArea(height: squareHeight, width: squareWidth)
 //result
 print("The area of the square is: \(area)")
 
+// Answer of Q1A2
+print("Enter a word to convert it to upper case")
+// New Variable input from user
+if let inputString = (readLine()){
+    //  Variable ConvertedString which calls allCap function
+    let convertedString = allCap(string: inputString)
+    print("Original string: \(inputString)")
+    print("Converted string: \(convertedString)")
+}else{
+    print("invalid input")
+}
+
 /* Created instance of rectangle class and printing perimter and color of rectangle */
 let myRectangle = Rectangle(height: 5, width: 10, color: "blue")
 print("The perimeter of rectangle is \(myRectangle.getPerimeter())")
